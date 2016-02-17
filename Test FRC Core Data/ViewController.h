d5f41296c0a1d026007472bb44226025a1f97be7
Zvigun Alexandr <mircamnia@ukr.net>
2016-02-17 18:45:48 +0200

    Initial Commit

---
+++ Test FRC Core Data/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test FRC Core Data
+//
+//  Created by Mac on 17.02.16.
+//  Copyright © 2016 Tsvigun Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test FRC Core Data/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test FRC Core Data
+//
+//  Created by Mac on 17.02.16.
+//  Copyright © 2016 Tsvigun Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
